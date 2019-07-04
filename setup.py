2dbcf64ae6ddfda072ebff1ce786f4f50436e7eb
Andrew Kurauchi <andrew.kurauchi@gmail.com>
2019-07-04 14:31:55 -0300

    Fixing setup.py to copy lib files

--- setup.py
+++ setup.py
@@ -3,10 +3,13 @@ from distutils.extension import Extension
 from Cython.Build import cythonize
 
 setup(
+    name='tobii_client',
+    description='Python wrapper for Tobii Stream API',
     ext_modules = cythonize([
         Extension('tobii_client', ['tobii_client.pyx'],
             include_dirs=['include'],
             libraries=['tobii_stream_engine'],
         )
-    ], language_level=3)
+    ], language_level=3),
+    data_files=[('', ['tobii_stream_engine.dll', 'tobii_stream_engine.lib'])]
 )
